dfbf84f8a415d5c53f5c2517e44ea2f26bfe836e
Nikhil Bhardwaj <47502228+BhardwajNkl@users.noreply.github.com>
2024-02-05 19:06:43 +0530

    Create customer-import.sql

---
+++ data-init/customer-import.sql
@@ -0,0 +1,3 @@
+insert into customer(name, age) values ('Nikhil Bhardwaj', 24);
+insert into customer(name, age) values ('Nishant Sharma', 22);
+insert into customer(name, age) values ('Buddha Naveen', 23);
