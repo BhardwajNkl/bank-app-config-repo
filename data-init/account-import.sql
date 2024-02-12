35e704f2473abe8ff590468068e0bd34d4c30789
Nikhil Bhardwaj <47502228+BhardwajNkl@users.noreply.github.com>
2024-02-12 16:12:04 +0530

    Delete data-init directory

--- data-init/account-import.sql
+++
@@ -1,3 +0,0 @@
-insert into account(customer_id, balance) values (1, 1000);
-insert into account(customer_id, balance) values (2, 500);
-insert into account(customer_id, balance) values (3, 1500);
--- data-init/customer-import.sql
+++
@@ -1,3 +0,0 @@
-insert into customer(name, age) values ('Nikhil Bhardwaj', 24);
-insert into customer(name, age) values ('Nishant Sharma', 22);
-insert into customer(name, age) values ('Buddha Naveen', 23);
